40ec145bd59957ac5c2433d29701e76a1cad3f3c
Aditya Ambre <ambreaditya19@gmail.com>
2025-03-02 23:07:12 +0530

    Developed remaining Teams Page displaying seperate teams in the matches & Integrated the respective data with Statistics alert

--- lib/screen/team_view/team_mobile.dart
+++ lib/screen/team_view/team_mobile.dart
@@ -32,118 +32,160 @@ class TeamMobile extends StatelessWidget {
   }
 
   Widget _body(BuildContext context, TeamViewModel viewModel) {
-    return Padding(
-      padding: const EdgeInsets.all(12.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(
-                child: InkWell(
-                  onTap: () => viewModel.onTapTeamSelection(0),
-                  child: Container(
-                    // color: Colors.red,
-                    decoration: BoxDecoration(
-                      color: viewModel.isTeamASelected
-                          ? const Color.fromARGB(255, 83, 155, 83) : const Color.fromARGB(125, 200, 200, 200),
-                      borderRadius: const BorderRadius.all(Radius.circular(10.0)),
-                    ),
-                    height: MediaQuery.of(context).size.width*0.40,
-                    child: Center(
-                      child: Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Text(viewModel.cricketModelApiList[viewModel.selectedIndex]
-                                ?.teams[viewModel.cricketModelApiList[viewModel.selectedIndex]
-                                ?.matchdetail?.teamAway]?.nameFull ?? "",
-                                style: TextStyle(
-                                    color: viewModel.isTeamASelected
-                                        ? ResColors.white : const Color.fromARGB(250, 45, 45, 45),
-                                    fontSize: 22,
-                                    fontWeight: FontWeight.w600
-                                ),
-                                overflow: TextOverflow.visible
-                            ),
-                            const SizedBox(height: 10),
-                            Text("(${viewModel.cricketModelApiList[viewModel.selectedIndex]
-                                ?.teams[viewModel.cricketModelApiList[viewModel.selectedIndex]
-                                ?.matchdetail?.teamAway]?.nameShort ?? ""})",
-                                style: TextStyle(
-                                    color: viewModel.isTeamASelected
-                                        ? ResColors.white : const Color.fromARGB(250, 45, 45, 45),
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.w500
-                                ),
-                                overflow: TextOverflow.visible
-                            ),
-                          ],
+    return SizedBox(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: InkWell(
+                    onTap: () => viewModel.onTapTeamSelection(0),
+                    child: Container(
+                      // color: Colors.red,
+                      decoration: BoxDecoration(
+                        color: viewModel.isTeamASelected
+                            ? const Color.fromARGB(255, 83, 155, 83) : const Color.fromARGB(125, 200, 200, 200),
+                        borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+                      ),
+                      height: MediaQuery.of(context).size.width*0.40,
+                      child: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(viewModel.cricketModelApiList[viewModel.selectedIndex]
+                                  ?.teams[viewModel.cricketModelApiList[viewModel.selectedIndex]
+                                  ?.matchdetail?.teamAway]?.nameFull ?? "",
+                                  style: TextStyle(
+                                      color: viewModel.isTeamASelected
+                                          ? ResColors.white : const Color.fromARGB(250, 45, 45, 45),
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.w600
+                                  ),
+                                  overflow: TextOverflow.visible
+                              ),
+                              const SizedBox(height: 10),
+                              Text("(${viewModel.cricketModelApiList[viewModel.selectedIndex]
+                                  ?.teams[viewModel.cricketModelApiList[viewModel.selectedIndex]
+                                  ?.matchdetail?.teamAway]?.nameShort ?? ""})",
+                                  style: TextStyle(
+                                      color: viewModel.isTeamASelected
+                                          ? ResColors.white : const Color.fromARGB(250, 45, 45, 45),
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w500
+                                  ),
+                                  overflow: TextOverflow.visible
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(width: 10),
-              Icon(Icons.compare_arrows_sharp, color: ResColors.iconColor),
-              const SizedBox(width: 10),
-              Expanded(
-                child: InkWell(
-                  onTap: () => viewModel.onTapTeamSelection(1),
-                  child: Container(
-                    // color: Colors.red,
-                    decoration: BoxDecoration(
-                      color: viewModel.isTeamBSelected
-                          ? const Color.fromARGB(255, 83, 155, 83) : const Color.fromARGB(125, 200, 200, 200),
-                      borderRadius: const BorderRadius.all(Radius.circular(10.0)),
-                    ),
-                    height: MediaQuery.of(context).size.width*0.40,
-                    child: Center(
-                      child: Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Text(viewModel.cricketModelApiList[viewModel.selectedIndex]
-                              ?.teams[viewModel.cricketModelApiList[viewModel.selectedIndex]
-                              ?.matchdetail?.teamHome]?.nameFull ?? "",
-                              style: TextStyle(
-                                color: viewModel.isTeamBSelected
-                                    ? ResColors.white : const Color.fromARGB(250, 45, 45, 45),
-                                fontSize: 22,
-                                fontWeight: FontWeight.w600
-                              ),
-                              overflow: TextOverflow.visible
-                            ),
-                            const SizedBox(height: 10),
-                            Text("(${viewModel.cricketModelApiList[viewModel.selectedIndex]
+                const SizedBox(width: 10),
+                Icon(Icons.compare_arrows_sharp, color: ResColors.iconColor),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: InkWell(
+                    onTap: () => viewModel.onTapTeamSelection(1),
+                    child: Container(
+                      // color: Colors.red,
+                      decoration: BoxDecoration(
+                        color: viewModel.isTeamBSelected
+                            ? const Color.fromARGB(255, 83, 155, 83) : const Color.fromARGB(125, 200, 200, 200),
+                        borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+                      ),
+                      height: MediaQuery.of(context).size.width*0.40,
+                      child: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(viewModel.cricketModelApiList[viewModel.selectedIndex]
                                 ?.teams[viewModel.cricketModelApiList[viewModel.selectedIndex]
-                                ?.matchdetail?.teamHome]?.nameShort ?? ""})",
+                                ?.matchdetail?.teamHome]?.nameFull ?? "",
                                 style: TextStyle(
                                   color: viewModel.isTeamBSelected
                                       ? ResColors.white : const Color.fromARGB(250, 45, 45, 45),
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w500
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w600
                                 ),
                                 overflow: TextOverflow.visible
-                            ),
-                          ],
+                              ),
+                              const SizedBox(height: 10),
+                              Text("(${viewModel.cricketModelApiList[viewModel.selectedIndex]
+                                  ?.teams[viewModel.cricketModelApiList[viewModel.selectedIndex]
+                                  ?.matchdetail?.teamHome]?.nameShort ?? ""})",
+                                  style: TextStyle(
+                                    color: viewModel.isTeamBSelected
+                                        ? ResColors.white : const Color.fromARGB(250, 45, 45, 45),
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w500
+                                  ),
+                                  overflow: TextOverflow.visible
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
+              ],
+            ),
+            Expanded(
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  return Padding(
+                    padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0),
+                    child: SizedBox(
+                        height: constraints.maxHeight,
+                      child: ListView.builder(
+                        itemCount: viewModel.teamMap![viewModel.isTeamASelected ? viewModel.cricketModelApiList[viewModel.selectedIndex]?.matchdetail?.teamHome : viewModel.cricketModelApiList[viewModel.selectedIndex]?.matchdetail?.teamAway]?.players.length,
+                        itemBuilder: (context, index) {
+                          viewModel.players.clear();
+                          viewModel.mapTeams();
+
+                          return Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  color: Colors.white,
+                                  width: constraints.maxWidth/1.05,
+                                  child: ListTile(
+                                    onTap: () => viewModel.showAlertDialog(context, index, viewModel.players[index].nameFull ?? "-"),
+                                    title: Text("Player: ${viewModel.players[index].nameFull ?? "-"} ${viewModel.players[index].iscaptain ?? false ? "(Captain)" : ""}"),
+                                    subtitle: Text("Position: ${viewModel.players[index].position ?? "-"}"),
+                                    trailing: Text(viewModel.players[index].iskeeper ?? false ? "(Wicket-Keeper)" : ""),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          );
+                        },),
+                    ),
+                  );
+                },
               ),
-            ],
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screen/team_view/team_view_model.dart
+++ lib/screen/team_view/team_view_model.dart
@@ -10,6 +10,8 @@ class TeamViewModel extends BaseViewModel {
   final List<CricketModel?> cricketModelApiList;
   final int selectedIndex;
   Map<String, Team>? teamMap;
+  List<Player> players = [];
+  List<Player>? playerList;
   bool isTeamASelected = true;
   bool isTeamBSelected = false;
 
@@ -37,6 +39,70 @@ class TeamViewModel extends BaseViewModel {
       isTeamBSelected = true;
     }
     notifyListeners();
+  }
+
+  void mapTeams() {
+    teamMap?.forEach((teamName, team) {
+      String selectedTeam = "${isTeamASelected ? cricketModelApiList[selectedIndex]?.matchdetail?.teamAway : cricketModelApiList[selectedIndex]?.matchdetail?.teamHome}";
+      if (teamName == selectedTeam) {
+        team.players.forEach((playerName, player) {
+          players.add(player);
+        });
+        return;
+      }
+    });
+
+    Logger.log("Players:", players.toString());
+  }
+
+  void showAlertDialog(BuildContext context, int index, String player) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text('Statistics', style: TextStyle(fontSize: 22),),
+              const SizedBox(height: 2.5),
+              Text('Player: $player', style: const TextStyle(fontSize: 16, fontStyle: FontStyle.italic)),
+              const Divider(),
+            ],
+          ),
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text("Batting", style: TextStyle(fontSize: 20)),
+                const SizedBox(height: 5.0),
+                Text("• Style: ${players[index].batting?.style}", style: const TextStyle(fontSize: 16)),
+                Text("• Average: ${players[index].batting?.average}", style: const TextStyle(fontSize: 16)),
+                Text("• Strike Rate: ${players[index].batting?.strikerate}", style: const TextStyle(fontSize: 16)),
+                Text("• Runs: ${players[index].batting?.runs}", style: const TextStyle(fontSize: 16)),
+                const SizedBox(height: 20.0),
+                const Text("Bowling", style: TextStyle(fontSize: 20)),
+                const SizedBox(height: 5.0),
+                Text("• Style: ${players[index].bowling?.style}", style: const TextStyle(fontSize: 16)),
+                Text("• Average: ${players[index].bowling?.average}", style: const TextStyle(fontSize: 16)),
+                Text("• Economy Rate: ${players[index].bowling?.economyrate}", style: const TextStyle(fontSize: 16)),
+                Text("• Wickets: ${players[index].bowling?.wickets}", style: const TextStyle(fontSize: 16)),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Get.back(),
+              child: const Text("EXIT"),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   void onBackClick() {
